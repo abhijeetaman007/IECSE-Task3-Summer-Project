ed742fc802bc9a490bfb0e07829b49553ba277fc
abhijeetaman007 <abhijeetaman007@gmail.com>
2020-06-30 18:07:23 +0530

    new routes added

--- Routes/playlist.js
+++ Routes/playlist.js
@@ -7,17 +7,69 @@ router.post('/add', async (req,res)=>{
     const myplaylist =new playlist({
         CreatedBy:req.body.createdby,
         Songs:req.body.songs,
-        Title:req.body.songs
+        Title:req.body.title
 
     })
  try{
     const savedplaylist= await myplaylist.save();
-    res.status(200).json(myplaylist);
+    res.status(200).json(savedplaylist);
  }
  catch(err){
     res.json({message: err});
  }
 });
+
+//To get all playlist
+router.get('/view/all',async (req,res)=>{
+    try{
+        const viewplaylist = await playlist.find();
+        res.json(viewplaylist)
+    }
+    catch(error){
+        res.json({message:err})
+    }
+})
+
+//To get a song using id
+router.get('/view/:playlistid',async(req,res)=>{
+    try{
+        const viewplaylist = await playlist.findById(req.params.playlistid) 
+        res.json(viewplaylist)
+    }
+    catch(error){
+        res.send(error)
+    }
+})
+
+//To update a playlist
+router.patch('/update/:playlistid',async (req,res)=>{
+    try{
+        const updatedplaylist= await playlist.updateOne(
+            {_id:req.params.playlistid},
+            {$set:{
+                CreatedBy:req.body.createdby,
+                Songs:req.body.songs,
+                Title:req.body.title
+            },
+        })
+        res.status(200).json(updatedplaylist)
+    }
+    catch(error){
+        res.json({message: error})
+    }
+})
+
+//To delete a playlist
+router.delete('/delete/:playlistid',async (req,res)=>{
+    try{
+        const deletedplaylist=await playlist.remove({_id:req.params.playlistid})
+        res.json(deletedplaylist)
+    }
+    catch(err){
+        res.json({message:err})
+    }
+})
+
 
 
 module.exports=router;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyparser=require('body-parser')
 const songroute =require('./Routes/songs')
 const playlistroute=require('./Routes/playlist')
 
-
+//Middleware for bodyparser:json
 app.use(bodyparser.json())
 
 //Connection to Database:MongoDB
@@ -25,100 +25,6 @@ app.use('/songs',songroute)
 
 //Middleware for playlist
 app.use('/playlist',playlistroute)
-
-
-
-// // app.post("/addsong",(req,res)=>{
-// //     const song= new songs({
-// //     Title: req.body.title,
-// //     Artist: req.body.artist,
-// //     Genre: req.body.genre,
-// //     Album: req.body.album,
-// //     });
-// //     song.save()
-// //     .then( data =>{
-// //         res.json(data)
-// //     })
-// //     .catch(err =>{
-// //         res.json({message: err})
-// //     })
-// // })
-
-// //To add Song
-// app.post('/addsong', async (req,res)=>{
-//     const song =new songs({
-//          Title: req.body.title,
-//          Artist: req.body.artist,
-//          Genre: req.body.genre,
-//          Album: req.body.album,
-//     })
-//  try{
-//     const savedsong= await song.save();
-//     res.status(200).json(savedsong);
-//  }
-//  catch(err){
-//     res.json({message: err});
-//  }
-// });
-
-// //To get all songs
-// app.get('/view/allsongs',async (req,res)=>{
-//     try{
-//         const viewsongs = await songs.find();
-//         //res.json(viewsongs)
-//         res.send(viewsongs)
-//     }
-//     catch(error){
-//         res.json({message:err})
-//     }
-// })
-
-// //To get a song using id
-// app.get('/view/:songid',async(req,res)=>{
-//     try{
-//         const viewsong = await songs.findById(req.params.songid) 
-//         res.json(viewsong)
-//     }
-//     catch(error){
-//         res.send(error)
-//     }
-// })
-
-// //To delete a song
-// app.delete('/delete/:songid',async (req,res)=>{
-//     try{
-//     const deltedsong=await songs.remove({_id:req.params.songid})
-//     res.json(deltedsong)
-//     }
-//     catch(error){
-//         res.json({message:error})
-//     }
-// })
-
-// //To update a song
-// app.patch('/update/:songid',async (req,res)=>{
-//     try{
-//         const updatedsong= await songs.updateMany(
-//             {_id:req.params.songid},
-//             {$set:{
-//                 Title:req.body.title,
-//                 Artist:req.body.artist,
-//                 Genre:req.body.genre,
-//                 Album:req.body.album,
-//             },
-//             //$set:{Artist:req.body.artist},
-//             //$set:{Genre:req.body.genre},
-//             //$set:{Album:req.body.album},
-//         })
-//         res.status(200).json(updatedsong)
-//     }
-//     catch(error){
-//         res.json({message: error})
-//     }
-// })
-
-
-
 
 //Listening at port:4000
  app.listen(4000,()=>{
